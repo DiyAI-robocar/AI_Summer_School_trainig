400ece6eac84b98ab8b365146c1c12b71d7ebf15
kurbakov <kurbakov.d@gmail.com>
2019-08-28 21:15:56 +0200

    WIP clean up

--- manage.py
+++ manage.py
@@ -5,10 +5,8 @@ Usage:
 
 """
 import os
-import time
 import types
 
-import numpy as np
 
 class Config:
     
@@ -65,14 +63,9 @@ def load_config(config_path=None):
         print("loading personal config over-rides")
         personal_cfg = Config()
         personal_cfg.from_pyfile(personal_cfg_path)
-        #personal_cfg.show()
 
         cfg.from_object(personal_cfg)
 
-        #print("final settings:")
-        #cfg.show()
-        
-    
     #derivative settings
     if hasattr(cfg, 'IMAGE_H') and hasattr(cfg, 'IMAGE_W'): 
         cfg.TARGET_H = cfg.IMAGE_H - cfg.ROI_CROP_TOP - cfg.ROI_CROP_BOTTOM
