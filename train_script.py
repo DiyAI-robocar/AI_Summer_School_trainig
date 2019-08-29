66efd05d40d5c989018af33ab1523b8892652069
Michal Slonina <michal.slonina@gmail.com>
2019-08-29 10:27:25 +0200

    removed get_deafult_model

--- train_script.py
+++ train_script.py
@@ -13,35 +13,9 @@ import os
 import random
 import time
 
-'''
-utils.py
-
-Functions that don't fit anywhere else.
-
-'''
-
-
-'''
-IMAGES
-'''
-
-def scale(im, size=128):
-    '''
-    accepts: PIL image, size of square sides
-    returns: PIL image scaled so sides lenght = size 
-    '''
-    size = (size,size)
-    im.thumbnail(size, Image.ANTIALIAS)
-    return im
 
 def normalize_and_crop(img_arr, cfg):
     img_arr = img_arr.astype(np.float32) / 255.0
-    if cfg.ROI_CROP_TOP or cfg.ROI_CROP_BOTTOM:
-        img_arr = img_crop(img_arr, cfg.ROI_CROP_TOP, cfg.ROI_CROP_BOTTOM)
-        if len(img_arr.shape) == 2:
-            img_arrH = img_arr.shape[0]
-            img_arrW = img_arr.shape[1]
-            img_arr = img_arr.reshape(img_arrH, img_arrW, 1)
     return img_arr
 
 
@@ -54,12 +28,9 @@ def load_scaled_image_arr(filename, cfg):
         img = Image.open(filename)
         if img.height != cfg.IMAGE_H or img.width != cfg.IMAGE_W:
             img = img.resize((cfg.IMAGE_W, cfg.IMAGE_H))
-        img_arr = np.array(img)
-        img_arr = normalize_and_crop(img_arr, cfg)
+        img_arr = np.array(img).astype(np.float32) / 255.0
         croppedImgH = img_arr.shape[0]
         croppedImgW = img_arr.shape[1]
-        if img_arr.shape[2] == 3 and cfg.IMAGE_DEPTH == 1:
-            img_arr = rgb2gray(img_arr).reshape(croppedImgH, croppedImgW, 1)
     except Exception as e:
         print(e)
         print('failed to load image:', filename)
@@ -123,26 +94,6 @@ def gather_records(cfg, tub_names, opts=None, verbose=False):
         records += record_paths
 
     return records
-
-def get_model_by_type(model_type, cfg):
-    '''
-    given the string model_type and the configuration settings in cfg
-    create a Keras model and return it.
-    '''
- 
-    if model_type is None:
-        model_type = cfg.DEFAULT_MODEL_TYPE
-    print("\"get_model_by_type\" model Type is: {}".format(model_type))
-
-    input_shape = (cfg.IMAGE_H, cfg.IMAGE_W, cfg.IMAGE_DEPTH)
-    roi_crop = (cfg.ROI_CROP_TOP, cfg.ROI_CROP_BOTTOM)
-
-    if model_type == "linear":
-        kl = KerasLinear(input_shape=input_shape, roi_crop=roi_crop)
-    else:
-        raise Exception("unknown model type: %s" % model_type)
-
-    return kl
 
 def train_test_split(data_list, shuffle=True, test_size=0.2):
     '''
@@ -564,7 +515,9 @@ def train(cfg, tub_names, model_name, transfer_model, model_type, continuous, au
     else:
         train_type = model_type
 
-    kl = get_model_by_type(train_type, cfg=cfg)
+    input_shape = (cfg.IMAGE_H, cfg.IMAGE_W, cfg.IMAGE_DEPTH)
+    roi_crop = (cfg.ROI_CROP_TOP, cfg.ROI_CROP_BOTTOM)
+    kl = KerasLinear(input_shape=input_shape, roi_crop=roi_crop)
 
     opts['categorical'] = False
 
